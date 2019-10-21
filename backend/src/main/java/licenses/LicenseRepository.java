20ce0fb7cc00616eed7672bb12e611c3a8915e59
Kike <kikevalps3@gmail.com>
2019-10-21 16:46:07 +0200

    Minor changes

--- backend/src/main/java/licenses/LicenseRepository.java
+++ backend/src/main/java/licenses/LicenseRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface LicenseRepository  extends JpaRepository<License, Long> {
-
+	License findBySerial(String serial);
 }
--- backend/src/main/java/licenses/LicenseService.java
+++ backend/src/main/java/licenses/LicenseService.java
@@ -1,5 +1,16 @@
 package licenses;
 
-public class LicenseService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+ class LicenseService {
+	
+	@Autowired
+	private LicenseRepository licRep;
+	
+	
+	public License findOne(String serial) {
+		return this.licRep.findBySerial(serial);
+	}
 }
