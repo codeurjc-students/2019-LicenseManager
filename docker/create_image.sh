183509dd90baaa6032ecfeefa90525a437d42a47
Kike <kikevalps3@gmail.com>
2020-02-24 21:43:57 +0100

    Update create_image.sh

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,32 +1,32 @@
 #! /bin/bash
-#cd ../frontend/dist
+cd ../frontend/dist
 
 #Delete current front file folder my-app
-#rm -r my-app
-#cd ..
-#docker run --rm --name angular-cli -v ${PWD}:/angular -w /angular node /bin/bash -c "npm install; npm run ng build --prod --baseHref=http://localhost:8080/"
+rm -r my-app
+cd ..
+sudo docker run --rm --name angular-cli -v ${PWD}:/angular -w /angular node /bin/bash -c "npm install; npm run ng build --prod --baseHref=http://localhost:8080/"
 
 #Delete current files on ssrc/main/resources/static (front files)
-#rm -rf ../backend/src/main/resources/static/*
-#cd dist
+rm -rf ../backend/src/main/resources/static/*
+cd dist
 
 #Move new front files to src/main/resources/static
-#mv my-app/* ../../backend/src/main/resources/static
+mv my-app/* ../../backend/src/main/resources/static
 
 
-#cd ../../backend
+cd ../../backend
 
-cd ../backend
+#cd ../backend
 
-docker run -it --rm -v ${PWD}:/usr/src/project -w /usr/src/project maven:alpine mvn package
+sudo docker run -it --rm -v ${PWD}:/usr/src/project -w /usr/src/project maven:alpine mvn package
 
 cp target/license-web-0.5.0.jar ../docker/build
 cd ../docker
 
-docker rmi -f kikeajani/licensoft
-docker build -t kikeajani/licensoft .
+sudo docker rmi -f kikeajani/licensoft
+sudo docker build -t kikeajani/licensoft .
 
 sudo docker login
 sudo docker push kikeajani/licensoft
 
-docker-compose up
+sudo docker-compose up
