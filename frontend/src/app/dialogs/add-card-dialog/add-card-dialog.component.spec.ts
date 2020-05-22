d16fc0e0aaa66237933c313f1168585d1bd39fd1
KikeAjani <kikevalps3@gmail.com>
2020-05-22 17:59:16 +0200

    AddCardDialogComponent tested

---
+++ frontend/src/app/dialogs/add-card-dialog/add-card-dialog.component.spec.ts
@@ -0,0 +1,117 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialog, MatSnackBar, MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { Observable, of } from 'rxjs';
+import { DOMHelper } from 'src/testing/dom-helper';
+import { User } from 'src/app/login/login.service';
+import { AddCardDialogComponent } from './add-card-dialog.component';
+
+describe('AddCardDialogComponent', () => {
+  let component: AddCardDialogComponent;
+  let fixture: ComponentFixture<AddCardDialogComponent>;
+  let routerMock:any;
+  let user:User={name:"Kike",roles:[],authdata:"",userStripeId:"stripe_id",email:"email@email.com"};
+  let domHelper:DOMHelper<AddCardDialogComponent>;
+  let dialogRefMock:any;
+
+  afterEach(()=>{
+    fixture.destroy();
+  })
+  
+  
+  beforeEach(async(() => {
+
+    class MatDialogRefMock {
+      close(value = '') {
+  
+      }
+    }
+
+    class MatDialogMock {
+      open() {
+          return {
+              afterClosed: () => of({ name: 'some object' })
+          };
+      }
+
+      close(){
+
+      }
+    }
+
+    dialogRefMock = jasmine.createSpyObj("DialogRef",["open","close"]);
+
+    dialogRefMock.open.and.returnValue(true);
+    dialogRefMock.close.and.returnValue(true);
+
+
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
+
+    TestBed.configureTestingModule({
+      declarations: [ AddCardDialogComponent ],
+      imports: [ MatDialogModule],
+
+      schemas:[NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {checkbox:true, noButton:true} },
+        {provide:MatDialogRef, useClass:MatDialogRefMock},
+        {provide:MatDialog, useClass:MatDialogMock},
+        {provide:MatSnackBar},
+        { provide: Router,useValue: routerMock},
+
+
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddCardDialogComponent);
+    component = fixture.componentInstance;
+    domHelper = new DOMHelper(fixture);
+  });
+
+
+  describe("Page creation-display", () => {
+      it("should create", () => {
+          fixture.detectChanges();
+
+
+      })
+  })
+
+
+  describe("Methods", () => {
+
+    it("should call yes()", () => {
+        spyOn(component.dialogRef,"close");
+        component.cardAdded=true;
+        fixture.detectChanges();
+        domHelper.clickButtonById("#yes-button");
+        expect(component.dialogRef.close).toHaveBeenCalledWith(true);
+    })
+
+    it("should call closeDialog()", () => {
+        spyOn(component.dialogRef,"close")
+        fixture.detectChanges();
+        domHelper.clickButtonById("#close-icon");
+        expect(component.dialogRef.close).toHaveBeenCalledWith(false);
+    })
+
+    it("should execute cardAddedHandler", () => {
+        fixture.detectChanges();
+        component.cardAddedHandler(true);
+        expect(component.cardAdded).toBe(true);
+    })
+
+  })
+
+
+
+  
+});
