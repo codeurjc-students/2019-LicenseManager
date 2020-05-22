dc8ab83e1052baa0bfaf4ad9eecba24c5fb04354
KikeAjani <kikevalps3@gmail.com>
2020-05-22 20:00:22 +0200

    All green coverage frontend

--- frontend/src/testing/dom-helper.ts
+++ frontend/src/testing/dom-helper.ts
@@ -9,11 +9,6 @@ export class DOMHelper<T>{
         this.fixture=fixture;
     }
 
-    count(tagName: string):number{
-        const elements = this.fixture.debugElement
-        .queryAll(By.css(tagName));
-        return elements.length;
-    }
 
     checkExists(tagName: string):boolean{
         return this.fixture.debugElement.query(By.css(tagName))!=null;
@@ -31,19 +26,6 @@ export class DOMHelper<T>{
         const buttonElement: HTMLButtonElement =this.fixture.debugElement.query(By.css(id)).nativeElement;
         
         buttonElement.click();
-    }
-
-    clickButton(buttonText:string){
-        this.findAll('button').forEach(button =>{
-            const buttonElement: HTMLButtonElement = button.nativeElement;
-            if(buttonElement.textContent === buttonText){
-                buttonElement.click();
-            }
-        });
-    }
-
-    findAll(tagName:string){
-        return this.fixture.debugElement.queryAll(By.css(tagName));
     }
 
     findAllButtonsWithName(buttonText:string){
